0279245b580db3094a48aca31ec9958b1d007010
andbudha <andreibartov@gmail.com>
2023-03-22 14:07:05 +0100

    img adding functionality added

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import bird from'./svg/bird.svg'
+import cat from'./svg/cat.svg'
+import dog from'./svg/dog.svg'
+import horse from'./svg/horse.svg'
+import gator from'./svg/gator.svg'
+import heart from'./svg/heart.svg'
+import cow from'./svg/cow.svg'
+
 const AnimalShow = ({type}) => {
-    return <div>{type}</div>
+
+    const svgMap = {bird, cat, dog, horse, gator, cow, heart};
+    return(
+        <div>
+            <img src={svgMap[type]} alt=""/>
+        </div>
+    );
 };
 
 export default AnimalShow;
