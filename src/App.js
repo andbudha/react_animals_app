ef3029fc51aa8cb97c946ff15e76c619c095365b
andbudha <andreibartov@gmail.com>
2023-03-22 10:19:23 +0100

    App updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import animalShow from "./AnimalShow";
+
+
+
+const getRandomAnimal = () => {
+    const animals = ['cow', 'horse', 'dog', 'cat', 'bird', 'gator'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+console.log(getRandomAnimal());
+
 
 const App = () => {
+
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+      setAnimals([...animals, getRandomAnimal()]);
+    }
+
+    
     return (
         <div>
-            Show animals here!
+            <button onClick={handleClick}>Add Animal</button>
+            <div>Animals to show: {animals}</div>
         </div>
     );
 };
